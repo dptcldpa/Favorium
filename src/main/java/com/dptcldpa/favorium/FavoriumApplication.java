7399f4de02a4edd5a27f8e4b42c532bb5a2c9969
조해민 <dptcldpa@gmail.com>
2024-12-22 22:36:02 +0900

    프로젝트 생성

---
+++ src/main/java/com/dptcldpa/favorium/FavoriumApplication.java
@@ -0,0 +1,13 @@
+package com.dptcldpa.favorium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoriumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavoriumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dptcldpa/favorium/FavoriumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dptcldpa.favorium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavoriumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
