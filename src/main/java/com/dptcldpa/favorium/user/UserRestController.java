2e9aaffb66be112bdf9187c3e31d887cabddd2f3
조해민 <dptcldpa@gmail.com>
2024-12-26 14:33:04 +0900

    회원가입 기능 구현 진행중

--- src/main/java/com/dptcldpa/favorium/user/UserRestController.java
+++ src/main/java/com/dptcldpa/favorium/user/UserRestController.java
@@ -1,13 +1,41 @@
 package com.dptcldpa.favorium.user;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dptcldpa.favorium.user.service.UserService;
+
 @RequestMapping("/user")
 @RestController
 public class UserRestController {
+	
+	@Autowired
+	private UserService userSerivce;
+	
+	// 회원가입
+	@PostMapping("/signup")
+	public Map<String, String> signup(
+			@RequestParam("loginId") String loginId
+			, @RequestParam("password") String password
+			, @RequestParam("email") String email
+			, @RequestParam("nickname") String nickname) {
+		
+		Map<String, String> resultMap = new HashMap<>();
+		
+		if(userSerivce.addUser(loginId, password, email, nickname)) {
+			resultMap.put("result", "success");
+		} else {
+			resultMap.put("result", "fail");
+		}
+		
+		return resultMap;
+	}
 	
 	// 로그인
 	@PostMapping("/login")
--- src/main/java/com/dptcldpa/favorium/user/service/UserService.java
+++ src/main/java/com/dptcldpa/favorium/user/service/UserService.java
@@ -3,6 +3,7 @@ package com.dptcldpa.favorium.user.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.dptcldpa.favorium.user.domain.User;
 import com.dptcldpa.favorium.user.repository.UserRepository;
 
 @Service
@@ -11,6 +12,23 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 	
-	
+	// 회원가입
+	public boolean addUser(String loginId, String password, String email, String nickname) {
+		
+		User user = User.builder()
+				.loginId(loginId)
+				.password(password)
+				.email(email)
+				.nickname(nickname)
+				.build();
+				
+		try {
+			userRepository.save(user);
+			return true;
+		} catch(Exception e) {
+			return false;
+		}
+		
+	}
 	
 }
