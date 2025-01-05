5dcbf9255a3186c546a67114b01ae62a32c80dae
조해민 <dptcldpa@gmail.com>
2025-01-05 20:57:52 +0900

    로그아웃 기능 구현 완료 (#3)

--- src/main/java/com/dptcldpa/favorium/user/UserController.java
+++ src/main/java/com/dptcldpa/favorium/user/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpSession;
+
 @RequestMapping("/user")
 @Controller
 public class UserController {
@@ -22,4 +24,14 @@ public class UserController {
 		return "user/login";
 	}
 	
+	// 로그아웃
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		
+		session.removeAttribute("userId");
+		session.removeAttribute("userLoginId");
+		
+		return "redirect:/user/login-view";
+			
+	}
 }
