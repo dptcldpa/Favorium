e74f4e9617e568ba1f2deb2fcdcb95531dbf421a
조해민 <dptcldpa@gmail.com>
2025-01-15 18:50:01 +0900

    게시글 전체보기 화면 구현 진행중

--- src/main/java/com/dptcldpa/favorium/post/PostController.java
+++ src/main/java/com/dptcldpa/favorium/post/PostController.java
@@ -2,13 +2,25 @@ package com.dptcldpa.favorium.post;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/post")
 @Controller
 public class PostController {
 	
-	@GetMapping("/post/create")
+	// 글 작성
+	@GetMapping("/create-view")
 	public String createPost() {
 		
 		return "post/create";
 	}
+	
+	// 전체보기
+	@GetMapping("/total-view")
+	public String totalPost() {
+		
+		return "post/total";
+	}
+	
+	
 }
