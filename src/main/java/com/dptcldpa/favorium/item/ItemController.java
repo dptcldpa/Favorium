5e58d743c55c60ce619e543392c70e747488a2ca
조해민 <dptcldpa@gmail.com>
2025-01-19 03:26:30 +0900

    게시글 보기 기능 진행중 - 글 추가 모달 분리한 것 한 곳에 합침

--- src/main/java/com/dptcldpa/favorium/item/ItemController.java
+++ src/main/java/com/dptcldpa/favorium/item/ItemController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.dptcldpa.favorium.category.dto.CategoryDTO;
 import com.dptcldpa.favorium.category.service.CategoryService;
+import com.dptcldpa.favorium.item.dto.PostDTO;
+import com.dptcldpa.favorium.item.service.ItemService;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -17,9 +19,11 @@ import jakarta.servlet.http.HttpSession;
 public class ItemController {
 	
 	private CategoryService categoryService;
+	private ItemService itemService;
 	
-	public ItemController(CategoryService categoryService) {
+	public ItemController(CategoryService categoryService, ItemService itemService) {
 		this.categoryService = categoryService;
+		this.itemService = itemService;
 	}
 	
 	// 글 작성
@@ -36,8 +40,10 @@ public class ItemController {
 		int userId = (Integer)session.getAttribute("userId");
 		
 		List<CategoryDTO> categoryList = categoryService.getCategoryList(userId);
+		List<PostDTO> postList = itemService.getPostList(userId);
 		
 		model.addAttribute("categoryList", categoryList);
+		model.addAttribute("postList", postList);
 		
 		return "post/total";
 	}
