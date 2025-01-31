3aa97791d449144715c97937175df28bc08bbf41
조해민 <dptcldpa@naver.com>
2025-02-01 00:50:05 +0900

    마이페이지에서 내가 만든 카테고리 목록이 보이는 기능 구현 (#7) - 가로 스크롤

--- src/main/java/com/dptcldpa/favorium/mypage/MyPageController.java
+++ src/main/java/com/dptcldpa/favorium/mypage/MyPageController.java
@@ -8,16 +8,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.dptcldpa.favorium.category.dto.CategoryDTO;
 import com.dptcldpa.favorium.category.service.CategoryService;
+import com.dptcldpa.favorium.item.dto.PostDTO;
+import com.dptcldpa.favorium.item.service.ItemService;
 
 import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class MyPageController {
 	
-private CategoryService categoryService;
+	private CategoryService categoryService;
+	private ItemService itemService;
 	
-	public MyPageController(CategoryService categoryService) {
+	public MyPageController(CategoryService categoryService, ItemService itemService) {
 		this.categoryService = categoryService;
+		this.itemService = itemService;
 	}
 
 	@GetMapping("/user/mypage-view")
@@ -26,8 +30,10 @@ private CategoryService categoryService;
 		int userId = (Integer)session.getAttribute("userId");
 		
 		List<CategoryDTO> categoryList = categoryService.getCategoryList(userId);
+		List<PostDTO> itemList = itemService.getPostList(userId);
 		
 		model.addAttribute("categoryList", categoryList);
+		model.addAttribute("itemList", itemList);
 		
 		return "mypage/mypage";
 		
